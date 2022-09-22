74a8fdfad1bf93331a49ffe2e5f8232ef79363ae
Polina <polina.morozova@geekprogram.org>
2022-09-22 11:26:17 +0500

    yfxfkj

---
+++ scripts/navigate-menu.js
@@ -0,0 +1,13 @@
+$(document).scroll(function(e) {
+    $(window).scrollTop() > 90? 
+    $('.navigate_menu').addClass('nav_color') : 
+    $('navigate_menu').removeClass('nav_color');
+
+});
+
+$(document).scroll(function(e) {
+    $(window).scrollTop() > 90 ? 
+    $('.header_logo').addClass('header_logo2') : 
+    $('header_logo').removeClass('header_logo2');
+
+});
