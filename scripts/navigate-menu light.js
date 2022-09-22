ce46e017fa4bc7b225b43a2660c41463c8569e01
Polina <polina.morozova@geekprogram.org>
2022-09-22 17:31:41 +0500

    Изменения

--- scripts/navigate-menu light.js
+++ scripts/navigate-menu light.js
@@ -1,12 +1,12 @@
 $(document).scroll(function(e) {
-    $(window).scrollTop() > 20? 
+    $(window).scrollTop() > 0? 
     $('.navigate_menu').addClass('nav_color1') : 
     $('navigate_menu').removeClass('nav_color1');
 
 });
 
 $(document).scroll(function(e) {
-    $(window).scrollTop() > 20 ? 
+    $(window).scrollTop() > 0 ? 
     $('.header_logo').addClass('header_logo2') : 
     $('header_logo').removeClass('header_logo2');
 
