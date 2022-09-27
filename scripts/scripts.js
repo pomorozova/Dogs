a4642c5081e3186c40c3436a755da459ffee3646
Polina <polina.morozova@geekprogram.org>
2022-09-27 13:06:26 +0500

    Админка

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,118 +1,111 @@
-function f1(){
-    // Кнопка по которой происходит клик
-    let callBackButton = $('#callback-button');
-   
-    // Модальное окно, которое необходимо открыть
-    let modal1 = $('#modal-1');
-   
-    // Кнопка "закрыть" внутри модального окна
-    let closeButton = $('#btn_close_modalAdd');
-   
-    // Тег body для запрета прокрутки
-    let tagBody = $('body');
-
-    $(callBackButton).on("click", function (e) {
-      e.preventDefault();
-      $(modal1).addClass('modal_active');
-      $(tagBody).addClass('hidden');
-    })
-    
-    $(closeButton).on("click", function (e) {
-      e.preventDefault();
-      $(modal1).removeClass('modal_active');
-      $(tagBody).removeClass('hidden');
-    })
-   
-    modal1.onmousedown = function (e) {
-      let target = e.target;
-      let modalContent = modal1.getElementsByClassName('modal__content')[0];
-      if (e.target.closest('.' + modalContent.className) === null) {
-        this.classList.remove('modal_active');
-        tagBody.classList.remove('hidden');
-      }
-    };
-       
-  }
-
-  function f2(){
-
-    // Кнопка по которой происходит клик
-    let callBackButton = $('.table-btn .but_change_note');
-   
-    // Модальное окно, которое необходимо открыть
-    let modal1 = $('#modal-2');
-   
-    // Кнопка "закрыть" внутри модального окна
-    let closeButton = $('#modal-2 .modal__close_button')[0];
-   
-    // Тег body для запрета прокрутки
-    let tagBody = $('body');
-    
-    console.log(callBackButton)
-    $(callBackButton).each(function(index, elem){
-      $(elem).on("click", function(e){
-        e.preventDefault();
-        $(modal1).addClass('modal_active');
-        $(tagBody).addClass('hidden');
-      })      
-    });
-    
-    
-    $(closeButton).on("click",function (e) {
-      e.preventDefault();
-      $(modal1).removeClass('modal_active');
-      $(tagBody).removeClass('hidden');
-    });
-   
-    modal1.onmousedown = function (e) {
-      let target = e.target;
-      let modalContent = modal1.getElementsByClassName('modal__content')[0];
-      if (e.target.closest('.' + modalContent.className) === null) {
-        this.classList.remove('modal_active');
-        tagBody.classList.remove('hidden');
-      }
-    };
-  }
-
-  function f3(){
-
-    // Кнопка по которой происходит клик
-    let callBackButton = document.getElementById('callback-button3');
-   
-    // Модальное окно, которое необходимо открыть
-    let modal1 = $('modal-3');
-   
-    // Кнопка "закрыть" внутри модального окна
-    let closeButton = $('.modal__close_button')[0];
-   
-    // Тег body для запрета прокрутки
-    let tagBody = document.getElementsByTagName('body');
-   
-    $(callBackButton).on("click", function (e) {
-      e.preventDefault();
-      $(modal1).addClass('modal_active');
-      $(tagBody).addClass('hidden');
-    })
-   
-    closeButton.onclick = function (e) {
-      e.preventDefault();
-      $(modal1).removeClass('modal_active');
-      $(tagBody).removeClass('hidden');
+$(function f4() {
+  $('#callback-button-delete').click(function () {
+    $('.modal-delete').addClass('modal_active');
+    $('body').addClass('hidden');
+  });
+ 
+  $('.modal__close-button').click(function () {
+    $('.modal-delete').removeClass('modal_active');
+    $('body').removeClass('hidden');
+  });
+ 
+  $('.modal-delete').mouseup(function (e) {
+    let modalContent = $(".modal__content");
+    if (!modalContent.is(e.target) && modalContent.has(e.target).length === 0) {
+      $(this).removeClass('modal_active');
+      $('body').removeClass('hidden');
     }
-    
-    $(modal1).on("onmousedown",function (e) {
-      let target = e.target;
-      let modalContent = modal1.getElementsByClassName('modal__content')[0];
-      if (e.target.closest('.' + modalContent.className) === null) {
-        this.classList.remove('modal_active');
-        tagBody.classList.remove('hidden');
+  });
+});
+
+$(function f5() {
+  $('#callback-button-delete-2').click(function () {
+    $('.modal-delete').addClass('modal_active');
+    $('body').addClass('hidden');
+  });
+ 
+  $('.modal__close-button').click(function () {
+    $('.modal-delete').removeClass('modal_active');
+    $('body').removeClass('hidden');
+  });
+ 
+  $('.modal-delete').mouseup(function (e) {
+    let modalContent = $(".modal__content");
+    if (!modalContent.is(e.target) && modalContent.has(e.target).length === 0) {
+      $(this).removeClass('modal_active');
+      $('body').removeClass('hidden');
+    }
+  });
+});
+
+
+
+  $(function f1() {
+    $('#callback-button').click(function () {
+      $('.modal').addClass('modal_active');
+      $('body').addClass('hidden');
+    });
+   
+    $('.modal__close-button').click(function () {
+      $('.modal').removeClass('modal_active');
+      $('body').removeClass('hidden');
+    });
+   
+    $('.modal').mouseup(function (e) {
+      let modalContent = $(".modal__content");
+      if (!modalContent.is(e.target) && modalContent.has(e.target).length === 0) {
+        $(this).removeClass('modal_active');
+        $('body').removeClass('hidden');
       }
     });
-  }
+  });
+       
+
+  $(function f2() {
+    $('#callback-button2').click(function () {
+      $('.modal-2').addClass('modal_active');
+      $('body').addClass('hidden');
+    });
+   
+    $('.modal__close-button').click(function () {
+      $('.modal-2').removeClass('modal_active');
+      $('body').removeClass('hidden');
+    });
+   
+    $('.modal-2').mouseup(function (e) {
+      let modalContent = $(".modal__content");
+      if (!modalContent.is(e.target) && modalContent.has(e.target).length === 0) {
+        $(this).removeClass('modal_active');
+        $('body').removeClass('hidden');
+      }
+    });
+  });
+
+  $(function f3() {
+    $('#callback-button3').click(function () {
+      $('.modal-3').addClass('modal_active');
+      $('body').addClass('hidden');
+    });
+   
+    $('.modal__close-button').click(function () {
+      $('.modal-3').removeClass('modal_active');
+      $('body').removeClass('hidden');
+    });
+   
+    $('.modal-3').mouseup(function (e) {
+      let modalContent = $(".modal__content");
+      if (!modalContent.is(e.target) && modalContent.has(e.target).length === 0) {
+        $(this).removeClass('modal_active');
+        $('body').removeClass('hidden');
+      }
+    });
+  });
 
   $(document).ready(function() { 
     f1(); 
     f2();
     f3();
+    f4();
+    f5();
   });
 
